f51d42484abeb00cedac9ca77213e347e90790e4
Charlye Arteaga <charlyearteaga@MacBook-Pro-de-Charlye.local>
2021-05-26 21:55:39 -0500

    changes

---
+++ Componentes/componentes/lib/src/pages/home_temp.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomePagesTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: _crearItemsCorta(),
+      ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+    for (var opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget)..add(Divider());
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.access_alarms),
+            trailing: ,//widget al incio
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+  }
+}
